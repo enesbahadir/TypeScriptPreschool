d938ba2c7f3ae25d5da9a782364a55bdbfb1333d
enesbahadir <enesbahadiryildirim@gmail.com>
2020-09-23 18:23:52 +0300

    1 file added

---
+++ Discount.ts
@@ -0,0 +1,48 @@
+class Discount {
+  
+  private _discountName : string;
+  private _discountType;
+  private _userTypes : Array<any>;
+  private _organizationName;
+  private _preschoolNamesAndTheirDiscounts :Array<string | number>;
+
+  get DiscountName() {
+    return this._discountName;
+  }
+
+  set DiscountName(discountName : string) {
+    this._discountName = discountName;
+  }
+
+  get DiscountType () {
+    return this._discountType;
+  }
+
+  set DiscountType(discountType) {
+    this._discountType = discountType;
+  }
+
+  get UserTypes () {
+    return this._userTypes;
+  }
+
+  set UserTypes(userTypes) {
+    this._userTypes = userTypes;
+  }
+
+  get OrganizationName () {
+    return this._organizationName;
+  }
+
+  set OrganizationName(organizationName) {
+    this._organizationName = organizationName;
+  }
+
+  get PreschoolNamesAndTheirDiscounts () {
+    return this._preschoolNamesAndTheirDiscounts;
+  }
+
+  set PreschoolNamesAndTheirDiscounts ( preschoolNamesAndTheirDiscounts) {
+    this._preschoolNamesAndTheirDiscounts = preschoolNamesAndTheirDiscounts;
+  }
+}
